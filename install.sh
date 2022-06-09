afb509a405722f536f7259a65adf066af53c9a15
Dillon Winchell <dwinchel@redhat.com>
2022-06-09 17:52:38 -0400

    Point at renamed install-platform.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ PLATFORM_OPS_DIR=${SCRIPT_DIR}/2-platform-ops
 [ ! -d ${PLATFORM_OPS_DIR} ] && git clone git@github.com:ploigos-automated-governance/2-platform-ops.git ${PLATFORM_OPS_DIR}
 
 # Run the platform-ops install script
-${PLATFORM_OPS_DIR}/install.sh
+${PLATFORM_OPS_DIR}/install-platform.sh
 
 # Delete LimitRanges for created namespaces if they exist
 oc delete limitrange --all -n devsecops
